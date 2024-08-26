8fd861137e9442495cbcc78f5bdc22f91556224c
organput <120797382+organput@users.noreply.github.com>
2024-08-26 16:38:20 +0800

    Update City.vue

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -37,7 +37,7 @@ export default {
   },
   methods: {
     getCityInfo () {
-      axios.get('/api/city.json')
+      axios.get('/mock/city.json')
         .then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
